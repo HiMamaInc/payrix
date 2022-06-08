22ba826e2c318d37cd7178adfdbb83010e92d0ae
Harry Lewis <harry.lewis@queensu.ca>
2022-06-08 12:02:29 -0400

    Update tests

--- spec/lib/payrix/resource/mixin/attr_casters_spec.rb
+++ spec/lib/payrix/resource/mixin/attr_casters_spec.rb
@@ -3,22 +3,24 @@ RSpec.describe Payrix::Resource::Mixin::AttrCasters do
     include Payrix::Resource::Mixin::AttrCasters
 
     def initialize(params)
-      super(params, [:status])
+      super(params, [:status, :amount])
 
       @resource_name = 'test'
     end
 
-    attr_accessor :status
-    attr_cast_to_integer :status
+    attr_accessor :status, :amount
+    attr_cast_to_integer :status, :amount
   end
 
-  let(:resource) { Payrix::Resource::Tests.new(status: '1') }
+  let(:resource) { Payrix::Resource::Tests.new(status: '1', amount: '500') }
 
-  it 'casts the specified `attribute` as integer' do
+  it 'casts the specified attributes as integers' do
     expect(resource.status).to eq 1
+    expect(resource.amount).to eq 500
   end
 
-  it 'exposes additional method, #`attribute`_raw_value, which returns the value pre-casting' do
+  it 'exposes additional methods, #..._raw_value, for all attributes, which returns the value pre-casting' do
     expect(resource.status_raw_value).to eq '1'
+    expect(resource.amount_raw_value).to eq '500'
   end
 end
