ec84697848f06333416756d0c82fe55ff1ae1d78
Harry Lewis <harry.lewis@queensu.ca>
2023-07-27 14:40:47 -0400

    Update Payrix configuration in test environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,4 @@ RSpec.configure do |config|
   config.expose_dsl_globally = false
 end
 
-Payrix.configure do |config|
-  config.set_test_mode(true)
-end
-
 WebMock.disable_net_connect!(allow: 'https://test-api.payrix.com/')
