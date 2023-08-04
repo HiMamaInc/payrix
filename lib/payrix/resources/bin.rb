fc42613a54d5eb400bfeb806f4505c09c56851f2
Harry Lewis <harry.lewis@queensu.ca>
2023-08-04 10:11:21 -0400

    Add more constants

---
+++ lib/payrix/resources/bin.rb
@@ -0,0 +1,10 @@
+module Payrix
+  class Bin
+    TYPE_CREDIT = 'credit'
+    TYPE_DEBIT = 'debit'
+    TYPE_DEBIT_OR_CREDIT = 'debitCredit'
+    TYPE_CHARGE = 'charge'
+    TYPE_PREPAID = 'prepaid'
+    TYPE_BANK_ACCOUNT = 'bankAccount'
+  end
+end
--- lib/payrix/resources/entity.rb
+++ lib/payrix/resources/entity.rb
@@ -1,5 +1,8 @@
 module Payrix
   class Entity < BaseResource
     RESOURCE_ENDPOINT = 'entities'
+
+    COUNTRY_USA = 'USA'
+    COUNTRY_CAN = 'CAN'
   end
 end
