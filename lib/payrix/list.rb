1d45f8bbd42f8b7c96b4a300c8a16b99a1b001c4
Harry Lewis <harry.lewis@queensu.ca>
2023-08-03 21:10:30 -0400

    Refactor Payrix::List

--- lib/payrix/list.rb
+++ lib/payrix/list.rb
@@ -4,80 +4,94 @@ module Payrix
 
     def initialize(klass:, filters: {}, options: {})
       @klass = klass
-      @api_key = options[:api_key] || Payrix.configuration.api_key
       @filters = filters
       @options = options
-      @search = Payrix::Search.construct(@filters)
-      @expand = Payrix::Expand.construct(options[:expand] || [])
-      @responses = []
-      @response = nil
-      @data = []
+      @current_page = 0
+      @current_data = []
+      @more = true
+
+      page_forward
     end
 
     def page_forward
-      paginate = Payrix::Paginate.construct(next_page)
+      @current_page += 1
 
-      json, status = Http::Request.instance.send_http(
-        'get',
-        Payrix.configuration.url,
-        "#{@klass::RESOURCE_ENDPOINT}?#{paginate}#{@expand}",
-        {},
-        {
-          'Content-Type' => 'application/json',
-          'APIKEY' => @api_key,
-          'SEARCH' => @search,
-        },
-      )
+      request_more
+    end
 
-      @response = Http::Response.new(json, status, @klass)
-      @responses.push(@response)
-      @data += @response.data.map do |resource|
-        Payrix::Object.new(resource, @klass.to_s.gsub('Payrix::', ''))
-      end
+    def page_backward
+      @current_page = [@current_page - 1, 1].max
+
+      request_more
     end
 
     def each(&block)
-      @data.each(&block)
+      @current_data.each(&block)
     end
 
     def auto_paging_each(&block)
+      @current_page = 0
+
       loop do
+        page_forward
         each(&block)
 
-        if more?
-          page_forward
-        else
-          break
-        end
+        break if !more?
       end
     end
 
     def auto_paging_map(&block)
-      auto_paging_each {}
+      collector = []
 
-      map(&block)
+      auto_paging_each { |record| collector.push(block.call(record)) }
+
+      collector
     end
 
     def auto_paging_count
-      auto_paging_each {}
+      count = 0
+
+      auto_paging_each { count += 1 }
 
       count
     end
 
     def more?
-      @response.nil? ? true : @response.has_more?
+      @more
     end
 
     def inspect
-      "#<#{self.class}:#{'0x0000%x' % (object_id << 1)} @klass=#{@klass}>"
+      "#<#{self.class}:#{'0x0000%x' % (object_id << 1)} resource=#{@klass} page=#{@current_page} count=#{@current_data.count}>"
     end
 
     private
 
-    def next_page
-      return 1 if @response.nil?
+    def request_more
+      paginate = Payrix::Paginate.construct(@current_page)
+      search = Payrix::Search.construct(@filters)
+      expand = Payrix::Expand.construct(@options[:expand] || [])
+      api_key = @options[:api_key] || Payrix.configuration.api_key
 
-      @response.page['current'] + 1
+      json, status = Http::Request.instance.send_http(
+        'get',
+        Payrix.configuration.url,
+        "#{@klass::RESOURCE_ENDPOINT}?#{paginate}#{expand}",
+        {},
+        {
+          'Content-Type' => 'application/json',
+          'APIKEY' => api_key,
+          'SEARCH' => search,
+        },
+      )
+
+      response = Http::Response.new(json, status, @klass)
+
+      @more = response.has_more?
+      @current_data = response.data.map do |resource|
+        Payrix::Object.new(resource, @klass.to_s.gsub('Payrix::', ''))
+      end
+
+      self
     end
   end
 end
