8c1e41eb8cedf157ea9331d9a66355f7b1113aa9
Harry Lewis <harry.lewis@queensu.ca>
2023-08-04 21:53:04 -0400

    Refactor Payrix::APIOperations::Retrieve and improve tests

--- lib/payrix/api_operations/retrieve.rb
+++ lib/payrix/api_operations/retrieve.rb
@@ -3,13 +3,16 @@ module Payrix
     module Retrieve
       def retrieve(id, options = {})
         if !id.is_a?(String)
-          raise Payrix::Exceptions::ResourceNotFound, "Couldn't find #{self} without ID"
+          raise ArgumentError, "#{self}.retrieve takes a string argument"
         end
 
         response = Payrix::Util.get(klass: self, filters: { id: id }, options: options)
 
         if response.data.first.nil?
-          raise Payrix::Exceptions::ResourceNotFound, "Couldn't find #{self} with id='#{id}'"
+          raise(
+            Payrix::Exceptions::ResourceNotFound,
+            "Couldn't find #{self} with id='#{id}'",
+          )
         end
 
         Payrix::Object.instantiate_from(response.data.first)
--- spec/lib/payrix/api_operations/retrieve_spec.rb
+++ spec/lib/payrix/api_operations/retrieve_spec.rb
@@ -6,87 +6,61 @@ RSpec.describe Payrix::APIOperations::Retrieve do
   end
 
   describe '.retrieve' do
-    context 'when passing a number argument' do
-      it 'raises Payrix::Exceptions::ResourceNotFound' do
-        expect { Txn.retrieve(1) }.to(
-          raise_error(Payrix::Exceptions::ResourceNotFound, "Couldn't find Txn without ID"),
+    context 'when passing nil' do
+      it 'raises ArgumentError' do
+        expect { Txn.retrieve(nil) }.to(
+          raise_error(ArgumentError, 'Txn.retrieve takes a string argument'),
         )
       end
     end
 
-    context 'when passing an array argument' do
-      it 'raises Payrix::Exceptions::ResourceNotFound' do
+    context 'when passing a number' do
+      it 'raises ArgumentError' do
+        expect { Txn.retrieve(0) }.to(
+          raise_error(ArgumentError, 'Txn.retrieve takes a string argument'),
+        )
+      end
+    end
+
+    context 'when passing a symbol' do
+      it 'raises ArgumentError' do
+        expect { Txn.retrieve(:symbol) }.to(
+          raise_error(ArgumentError, 'Txn.retrieve takes a string argument'),
+        )
+      end
+    end
+
+    context 'when passing an array' do
+      it 'raises ArgumentError' do
         expect { Txn.retrieve([]) }.to(
-          raise_error(Payrix::Exceptions::ResourceNotFound, "Couldn't find Txn without ID"),
+          raise_error(ArgumentError, 'Txn.retrieve takes a string argument'),
         )
       end
     end
 
-    context 'when passing a hash argument' do
-      it 'raises Payrix::Exceptions::ResourceNotFound' do
+    context 'when passing a hash' do
+      it 'raises ArgumentError' do
         expect { Txn.retrieve({}) }.to(
-          raise_error(Payrix::Exceptions::ResourceNotFound, "Couldn't find Txn without ID"),
+          raise_error(ArgumentError, 'Txn.retrieve takes a string argument'),
         )
       end
     end
 
-    context 'when passing a string argument' do
-      it 'raises Payrix::Exceptions::ResourceNotFound if the API returns an empty response' do
-        mock_request(
-          url: 'https://api.payrix.com/txns',
-          id: 't1_txn_64026b07cc6a79dd5cfd0da',
-          data: [],
-        )
-
-        expect { Txn.retrieve('t1_txn_64026b07cc6a79dd5cfd0da') }.to(
-          raise_error(Payrix::Exceptions::ResourceNotFound, "Couldn't find Txn with id='t1_txn_64026b07cc6a79dd5cfd0da'"),
-        )
-      end
-
-      it 'returns Payrix resource instance if the API returns a non-empty response' do
-        mock_request(
-          url: 'https://api.payrix.com/txns',
-          id: 't1_txn_64026b07cc6a79dd5cfd0da',
-          data: [{ id: 't1_txn_64026b07cc6a79dd5cfd0da', status: 1 }],
-        )
-
-        txn = Txn.retrieve('t1_txn_64026b07cc6a79dd5cfd0da')
-
-        expect(txn.id).to eq('t1_txn_64026b07cc6a79dd5cfd0da')
-        expect(txn.status).to eq(1)
-      end
-    end
-
-    context 'when passing an options hash as the second parameter' do
-      it 'supports passing an :expand key to use the expand API feature' do
-        mock_request(
-          url: 'https://api.payrix.com/txns?expand[merchant][]=',
-          id: 't1_txn_64026b07cc6a79dd5cfd0da',
-          data: [{ id: 't1_txn_64026b07cc6a79dd5cfd0da', status: 1 }],
-        )
-
-        txn = Txn.retrieve('t1_txn_64026b07cc6a79dd5cfd0da', { expand: ['merchant'] })
-
-        expect(txn.id).to eq('t1_txn_64026b07cc6a79dd5cfd0da')
-        expect(txn.status).to eq(1)
-      end
-
-      it 'supports passing an :api_key to authenticate the request' do
-        stub =
+    context 'when passing a string' do
+      context 'when the API response is empty' do
+        it 'raises Payrix::Exceptions::ResourceNotFound' do
           WebMock
             .stub_request(:get, 'https://api.payrix.com/txns')
             .with(
               headers: {
-                'Content-Type' => 'application/json',
-                'Apikey' => 'my-personal-key',
-                'Search' => 'id[equals]=t1_txn_64026b07cc6a79dd5cfd0da&',
+                'Search' => "id[equals]=t1_txn_64026b07cc6a79dd5cfd0da&",
               },
             )
             .to_return(
               status: 200,
               body: {
                 response: {
-                  data: [{}],
+                  data: [],
                   details: {
                     requestId: 1,
                     totals: [],
@@ -101,42 +75,132 @@ RSpec.describe Payrix::APIOperations::Retrieve do
               }.to_json,
             )
 
-        Txn.retrieve('t1_txn_64026b07cc6a79dd5cfd0da', { api_key: 'my-personal-key' })
+            expect { Txn.retrieve('t1_txn_64026b07cc6a79dd5cfd0da') }.to(
+              raise_error(
+                Payrix::Exceptions::ResourceNotFound,
+                "Couldn't find Txn with id='t1_txn_64026b07cc6a79dd5cfd0da'",
+              ),
+            )
+        end
+      end
 
-        expect(stub).to have_been_requested
+      context 'when the API response is not empty' do
+        it 'returns an object' do
+          WebMock
+            .stub_request(:get, 'https://api.payrix.com/txns')
+            .with(
+              headers: {
+                'Search' => "id[equals]=t1_txn_64026b07cc6a79dd5cfd0da&",
+              },
+            )
+            .to_return(
+              status: 200,
+              body: {
+                response: {
+                  data: [
+                    {
+                      id: 't1_txn_64026b07cc6a79dd5cfd0da',
+                      status: 1,
+                    },
+                  ],
+                  details: {
+                    requestId: 1,
+                    totals: [],
+                    page: {
+                      current: 1,
+                      last: 1,
+                      hasMore: false,
+                    },
+                  },
+                  errors: [],
+                },
+              }.to_json,
+            )
+
+          txn = Txn.retrieve('t1_txn_64026b07cc6a79dd5cfd0da')
+
+          expect(txn.id).to eq('t1_txn_64026b07cc6a79dd5cfd0da')
+          expect(txn.status).to eq(1)
+        end
+      end
+
+      context 'when passing an options hash' do
+        it 'supports resource expansion via the :expand key' do
+          WebMock
+            .stub_request(:get, 'https://api.payrix.com/txns?expand[merchant][]=')
+            .with(
+              headers: {
+                'Search' => "id[equals]=t1_txn_64026b07cc6a79dd5cfd0da&",
+              },
+            )
+            .to_return(
+              status: 200,
+              body: {
+                response: {
+                  data: [
+                    {
+                      id: 't1_txn_64026b07cc6a79dd5cfd0da',
+                      status: 1,
+                      merchant: {
+                        id: 't1_mer_64026b07cc6a79dd5cf77d7',
+                      },
+                    },
+                  ],
+                  details: {
+                    requestId: 1,
+                    totals: [],
+                    page: {
+                      current: 1,
+                      last: 1,
+                      hasMore: false,
+                    },
+                  },
+                  errors: [],
+                },
+              }.to_json,
+            )
+
+          txn = Txn.retrieve('t1_txn_64026b07cc6a79dd5cfd0da', { expand: ['merchant'] })
+
+          expect(txn.id).to eq('t1_txn_64026b07cc6a79dd5cfd0da')
+          expect(txn.status).to eq(1)
+          expect(txn.merchant.id).to eq('t1_mer_64026b07cc6a79dd5cf77d7')
+        end
+
+        it 'supports setting the API key per-request via the :api_key key' do
+          stub =
+            WebMock
+              .stub_request(:get, 'https://api.payrix.com/txns')
+              .with(
+                headers: {
+                  'Apikey' => 'my-personal-key',
+                  'Search' => 'id[equals]=t1_txn_64026b07cc6a79dd5cfd0da&',
+                },
+              )
+              .to_return(
+                status: 200,
+                body: {
+                  response: {
+                    data: [{}],
+                    details: {
+                      requestId: 1,
+                      totals: [],
+                      page: {
+                        current: 1,
+                        last: 1,
+                        hasMore: false,
+                      },
+                    },
+                    errors: [],
+                  },
+                }.to_json,
+              )
+
+          Txn.retrieve('t1_txn_64026b07cc6a79dd5cfd0da', { api_key: 'my-personal-key' })
+
+          expect(stub).to have_been_requested
+        end
       end
     end
-  end
-
-  def mock_request(url:, id:, data:)
-    allow(Payrix.configuration).to receive(:api_key).and_return('7E57C3FD328F8B00B2A72144FE2A1F83')
-
-    WebMock
-      .stub_request(:get, url)
-      .with(
-        headers: {
-          'Content-Type' => 'application/json',
-          'Apikey' => '7E57C3FD328F8B00B2A72144FE2A1F83',
-          'Search' => "id[equals]=#{id}&",
-        },
-      )
-      .to_return(
-        status: 200,
-        body: {
-          response: {
-            data: data,
-            details: {
-              requestId: 1,
-              totals: [],
-              page: {
-                current: 1,
-                last: 1,
-                hasMore: false,
-              },
-            },
-            errors: [],
-          },
-        }.to_json,
-      )
   end
 end
