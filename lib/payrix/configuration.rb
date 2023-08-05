2d497b0ce835cc57ed560a4d14f3844fbdcffa45
Harry Lewis <harry.lewis@queensu.ca>
2023-08-04 22:16:04 -0400

    Refactor Payrix::Configuration

--- lib/payrix/configuration.rb
+++ lib/payrix/configuration.rb
@@ -2,26 +2,23 @@ require 'uri'
 
 module Payrix
   class Configuration
-    attr_reader :url
     attr_accessor :api_key, :session_key
 
     def initialize
-      @url = 'https://api.payrix.com'
       @api_key = ''
       @session_key = ''
+      @test_mode = false
     end
 
-    def set_test_mode(test)
-      host = URI.parse(url).host
+    def set_test_mode(test_mode)
+      @test_mode = test_mode
+    end
 
-      if test == true
-        if !host.start_with?('test-')
-          @url = "https://test-#{host}"
-        end
+    def url
+      if @test_mode
+        'https://test-api.payrix.com'
       else
-        if host.start_with?('test-')
-          @url = "https://#{host[5..-1]}"
-        end
+        'https://api.payrix.com'
       end
     end
   end
