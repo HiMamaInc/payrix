07c7bd1b1e75e334409bf42204c59c14a4d49c17
Harry Lewis <harry.lewis@queensu.ca>
2023-08-07 22:52:51 -0400

    Refactor Payrix::Search to be Payrix::RequestOptions::Search

--- lib/payrix/request_options/search.rb
+++ lib/payrix/request_options/search.rb
@@ -7,71 +7,33 @@ require 'payrix/request_options/search/greater'
 require 'payrix/request_options/search/less'
 
 module Payrix
-  module Search
-    def self.construct(filter)
-      search = ''
+  module RequestOptions
+    module Search
+      def self.construct(filter)
+        case filter
+        when Hash
+          return '' if filter.empty?
 
-      case filter
-      when Hash
-        return search if filter.empty?
-
-        filter.each do |key, value|
-          search += Payrix::Search.equals(key, value).construct
+          filter
+            .map { |key, value| equals(key, value).construct }
+            .join('&')
+        when Node
+          filter.construct
+        else
+          raise ArgumentError, 'Search parameter must be a hash or an instance of Payrix::Search::Node'
         end
-      when Payrix::Search::Node
-        search += filter.construct
-      else
-        raise ArgumentError, 'Search parameter must be a hash or an instance of Payrix::Search::Node'
       end
 
-      search
-    end
-
-    def self.equals(field, value)
-      Equals.new(field, value)
-    end
-
-    def self.greater(field, value)
-      Greater.new(field, value)
-    end
-
-    def self.less(field, value)
-      Less.new(field, value)
-    end
-
-    class Node
-      def initialize(field, operator, value)
-        @field = field
-        @operator = operator
-        @value = value
+      def self.equals(field, value)
+        Equals.new(field, value)
       end
 
-      def construct
-        "#{@field}[#{@operator}]=#{@value}&"
+      def self.greater(field, value)
+        Greater.new(field, value)
       end
-    end
 
-    class Equals < Node
-      def initialize(field, value)
-        value = "''" if ['', nil].include?(value)
-
-        super(field, 'equals', value)
-      end
-    end
-
-    class Greater < Node
-      def initialize(field, value)
-        value = "''" if ['', nil].include?(value)
-
-        super(field, 'greater', value)
-      end
-    end
-
-    class Less < Node
-      def initialize(field, value)
-        value = "''" if ['', nil].include?(value)
-
-        super(field, 'less', value)
+      def self.less(field, value)
+        Less.new(field, value)
       end
     end
   end
--- spec/lib/payrix/request_options/search_spec.rb
+++ spec/lib/payrix/request_options/search_spec.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-RSpec.describe Payrix::Search do
+RSpec.describe Payrix::RequestOptions::Search do
   describe '.construct' do
-    context 'when passing a number argument' do
+    context 'when passing nil' do
       it 'raises ArgumentError' do
-        expect { described_class.construct(0) }.to(
+        expect { described_class.construct(nil) }.to(
           raise_error(ArgumentError, 'Search parameter must be a hash or an instance of Payrix::Search::Node'),
         )
       end
     end
 
-    context 'when passing an array argument' do
+    context 'when passing a number' do
       it 'raises ArgumentError' do
-        expect { described_class.construct([]) }.to(
+        expect { described_class.construct(0) }.to(
           raise_error(ArgumentError, 'Search parameter must be a hash or an instance of Payrix::Search::Node'),
         )
       end
@@ -26,65 +26,68 @@ RSpec.describe Payrix::Search do
       end
     end
 
-    context 'when passing an empty hash argument' do
-      it 'returns an empty string' do
-        expect(described_class.construct({})).to eq('')
-      end
-    end
-
-    context 'when passing a non-empty hash argument' do
-      it 'returns a search string using the equals operator' do
-        expect(described_class.construct({ status: 2, authorized: 1 })).to(
-          eq('status[equals]=2&authorized[equals]=1&'),
+    context 'when passing an array argument' do
+      it 'raises ArgumentError' do
+        expect { described_class.construct([]) }.to(
+          raise_error(ArgumentError, 'Search parameter must be a hash or an instance of Payrix::Search::Node'),
         )
       end
     end
 
-    context 'when passing an instance of Payrix::Search::Node' do
-      it 'returns a search string using the node operator' do
-        node = described_class.greater(:status, 1)
+    context 'when passing an empty hash argument' do
+      it 'returns an empty string' do
+        filter = {}
 
-        expect(described_class.construct(node)).to eq('status[greater]=1&')
+        expect(described_class.construct(filter)).to eq('')
+      end
+    end
+
+    context 'when passing a non-empty hash argument' do
+      it 'returns a search string of chained equals operators' do
+        filter = { status: 2, authorized: 1 }
+
+        expect(described_class.construct(filter)).to(
+          eq('status[equals]=2&authorized[equals]=1'),
+        )
+      end
+    end
+
+    context 'when passing an search node' do
+      it 'returns a search string using the node' do
+        node = described_class::Greater.new(:status, 1)
+
+        expect(described_class.construct(node)).to eq('status[greater]=1')
       end
     end
   end
 
   describe '.equals' do
-    it 'returns an object that can be used to #construct a string for API use' do
-      expect(described_class.equals('field', 'value').construct).to eq('field[equals]=value&')
-      expect(described_class.equals('field', 0).construct).to eq('field[equals]=0&')
-      expect(described_class.equals('field', '').construct).to eq("field[equals]=''&")
-      expect(described_class.equals('field', nil).construct).to eq("field[equals]=''&")
-      expect(described_class.equals(:field, 'value').construct).to eq('field[equals]=value&')
-      expect(described_class.equals(:field, 0).construct).to eq('field[equals]=0&')
-      expect(described_class.equals(:field, '').construct).to eq("field[equals]=''&")
-      expect(described_class.equals(:field, nil).construct).to eq("field[equals]=''&")
+    it 'returns an Equals search node instance with the pass in arguments' do
+      equals = described_class.equals(:field, 'value')
+
+      expect(equals).to be_a(described_class::Equals)
+      expect(equals.field).to eq(:field)
+      expect(equals.value).to eq('value')
     end
   end
 
   describe '.greater' do
-    it 'returns an object that can be used to #construct a string for API use' do
-      expect(described_class.greater('field', 'value').construct).to eq('field[greater]=value&')
-      expect(described_class.greater('field', 0).construct).to eq('field[greater]=0&')
-      expect(described_class.greater('field', '').construct).to eq("field[greater]=''&")
-      expect(described_class.greater('field', nil).construct).to eq("field[greater]=''&")
-      expect(described_class.greater(:field, 'value').construct).to eq('field[greater]=value&')
-      expect(described_class.greater(:field, 0).construct).to eq('field[greater]=0&')
-      expect(described_class.greater(:field, '').construct).to eq("field[greater]=''&")
-      expect(described_class.greater(:field, nil).construct).to eq("field[greater]=''&")
+    it 'returns a Greater search node instance with the pass in arguments' do
+      greater = described_class.greater(:field, 'value')
+
+      expect(greater).to be_a(described_class::Greater)
+      expect(greater.field).to eq(:field)
+      expect(greater.value).to eq('value')
     end
   end
 
   describe '.less' do
-    it 'returns an object that can be used to #construct a string for API use' do
-      expect(described_class.less('field', 'value').construct).to eq('field[less]=value&')
-      expect(described_class.less('field', 0).construct).to eq('field[less]=0&')
-      expect(described_class.less('field', '').construct).to eq("field[less]=''&")
-      expect(described_class.less('field', nil).construct).to eq("field[less]=''&")
-      expect(described_class.less(:field, 'value').construct).to eq('field[less]=value&')
-      expect(described_class.less(:field, 0).construct).to eq('field[less]=0&')
-      expect(described_class.less(:field, '').construct).to eq("field[less]=''&")
-      expect(described_class.less(:field, nil).construct).to eq("field[less]=''&")
+    it 'returns a Less search node instance with the pass in arguments' do
+      less = described_class.less(:field, 'value')
+
+      expect(less).to be_a(described_class::Less)
+      expect(less.field).to eq(:field)
+      expect(less.value).to eq('value')
     end
   end
 end
