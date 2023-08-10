20658dce06e5f82143dbdf20579912cc2e1f07ab
Harry Lewis <harry.lewis@queensu.ca>
2023-08-10 11:43:03 -0400

    Improve feel of Payrix::ApiError

--- lib/payrix/errors.rb
+++ lib/payrix/errors.rb
@@ -10,9 +10,24 @@ module Payrix
     def initialize(message = nil, object = {}, errors = [])
       @message = message
       @object = Object.instantiate_from(object)
+      @_errors = errors
       @errors = Object.instantiate_from(errors)
 
-      super(message)
+      super(
+        <<~ERROR
+          #{@message}
+
+            #{JSON.pretty_generate(Payrix::Util.recursive_snake_case(errors)).gsub("\n", "\n  ")}
+        ERROR
+      )
+    end
+
+    def inspect
+      parts = ["#<#{self.class}:#{format('0x0000%x', (object_id << 1))}"]
+
+      parts.append(JSON.pretty_generate(Payrix::Util.recursive_snake_case(@_errors)))
+
+      "#{parts.join(' ')}>".dup
     end
   end
 
