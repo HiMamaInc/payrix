8bde2a98ed03029736b90ea12bd0f7910e97a3dd
Harry Lewis <harry.lewis@queensu.ca>
2023-08-21 09:22:04 -0400

    Refactor specs for #send_http

--- spec/lib/payrix/http/request_spec.rb
+++ spec/lib/payrix/http/request_spec.rb
@@ -2,20 +2,6 @@
 
 RSpec.describe Payrix::Http::Request do
   describe '#send_http' do
-    let(:response_body) { '{}' }
-
-    before do
-      stub_request(:get, 'https://test-api.payrix.com/test')
-        .with(
-          headers: {
-            'Accept' => '*/*',
-            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'User-Agent' => 'Faraday v2.0.1',
-          },
-        )
-        .to_return(status: 200, body: response_body, headers: {})
-    end
-
     context 'when the request is not authenticated' do
       it 'raises Payrix::InvalidAuthenticationError' do
         WebMock
@@ -41,9 +27,18 @@ RSpec.describe Payrix::Http::Request do
     end
 
     context 'when response is valid json' do
-      let(:response_body) { '{}' }
-
       it 'handles the response' do
+        WebMock
+          .stub_request(:get, 'https://test-api.payrix.com/test')
+          .with(
+            headers: {
+              'Accept' => '*/*',
+              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'User-Agent' => 'Faraday v2.0.1',
+            },
+          )
+          .to_return(status: 200, body: '{}', headers: {})
+
         json, status = described_class.instance.send_http('get', 'https://test-api.payrix.com', 'test', {}, {}, 30)
         expect(json).to eq({})
         expect(status).to eq(200)
@@ -51,9 +46,18 @@ RSpec.describe Payrix::Http::Request do
     end
 
     context 'when request is invalid json' do
-      let(:response_body) { '' }
-
       it 'raises an exception' do
+        WebMock
+          .stub_request(:get, 'https://test-api.payrix.com/test')
+          .with(
+            headers: {
+              'Accept' => '*/*',
+              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'User-Agent' => 'Faraday v2.0.1',
+            },
+          )
+          .to_return(status: 200, body: '', headers: {})
+
         expect do
           described_class.instance.send_http('get', 'https://test-api.payrix.com', 'test', {}, {}, 30)
         end.to raise_error(Payrix::Exceptions::InvalidResponse)
