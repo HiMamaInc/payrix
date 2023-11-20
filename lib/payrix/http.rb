69e85c82a48ca8fbacc748218f4e676ac1bca60b
Harry Lewis <harry.lewis@queensu.ca>
2023-11-20 09:52:43 -0500

    Remove unused RequestParams

--- lib/payrix/http.rb
+++ lib/payrix/http.rb
@@ -2,7 +2,6 @@
 
 module Payrix
   module Http # rubocop:disable Style/Documentation - Legacy file, which will be removed eventually
-    autoload :RequestParams, 'payrix/http/request_params'
     autoload :Request, 'payrix/http/request'
     autoload :Response, 'payrix/http/response'
   end
